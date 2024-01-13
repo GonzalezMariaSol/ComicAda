a3ec774333197d78f977c77316f8ee42420fcec6
MariaSolGonzalez <solgonzalez24@hotmail.com>
2024-01-13 19:13:22 -0300

    finished the functionality for the page controls

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
+// render functions
 const just = (selector) => document.querySelector(selector);
 
+
+// universal functions for general code
 const urlBase = `http://gateway.marvel.com/v1/public/`;
 let ts = `ts=1`;
 const publicKey = "&apikey=d3abb539098557030e53849b9dc73d81";
 const hash = "&hash=56d3e07a1a8a5d3e0602e841365c58f5";
 
-const getMarvelComics = async () => {
-  // let existTitle = title? `&titleStartsWith=${title}` :""
-  const url = `${urlBase}comics?${ts}${publicKey}${hash}`; //${existTitle}`
-  console.log(url);
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data.data.results);
-  return data.data.results;
+
+// universal functions for pagination
+let offsetCounter = 0;
+let limitCounter = 20;
+let pageCounter = 1
+const itemsPerPage = 20
+let totalItems 
+
+
+
+
+
+
+//                             desde , hasta
+const getMarvelComics = async (offset, limit, pageNum) => {
+    // let existTitle = title? `&titleStartsWith=${title}` : ""
+    const url = `${urlBase}comics?${ts}&offset=${offset}&limit=${limit}${publicKey}${hash}`; //${existTitle}`
+    const response = await fetch(url);
+    const data = await response.json();
+    totalItems = data.data.total
+    console.log(totalItems)
+    return data;
 };
 
-const printComic = async () => {
-  const comics = await getMarvelComics();
-  just(".results-cards-comics").innerHTML = ``;
 
-  for (let comic of comics) {
+
+const printComic = async (offset, limit, pageNum) => {
+  const comics = await getMarvelComics(offset, limit, pageNum);
+  just(".results-cards-comics").innerHTML = ``;
+  just(".total-of-comics").innerText = `${comics.data.total} RESULTADOS`;
+  just(".current-page-num").innerText = `page ${pageNum}`
+
+
+  for (let comic of comics.data.results) {
     let pathImg = comic.thumbnail.path + "/portrait_incredible.jpg";
 
     just(".results-cards-comics").innerHTML += `
@@ -32,14 +54,48 @@ const printComic = async () => {
         `;
   }
 };
-printComic();
+printComic(offsetCounter, limitCounter, pageCounter);
 
-// const tryingToGetThePic = async() => {
-//     const comics = await getMarvelComics() //ARRA
-//     for(const comic of comics){
-//         let pathImg = comic.thumbnail.path + "/portrait_medium.jpg"
-//         // pathImg += "/portrait_medium.jpg"
-//         console.log(pathImg);
-//     }
-// }
-// tryingToGetThePic()
+
+//PAGINACION
+just(".btns-first-page").addEventListener("click", ()=> {
+    offsetCounter = 0
+    limitCounter = 20
+    pageCounter = 1
+    printComic(offsetCounter, limitCounter, pageCounter); 
+})
+
+just(".btns-next-page").addEventListener("click", () => {
+    if (offsetCounter + itemsPerPage < totalItems) {
+        offsetCounter += itemsPerPage;
+        limitCounter = Math.min(itemsPerPage, totalItems - offsetCounter);
+        pageCounter +=1
+    }
+        printComic(offsetCounter, limitCounter, pageCounter); 
+});
+
+
+
+just(".btns-prev-pag").addEventListener("click", () => {
+    if (offsetCounter >= itemsPerPage) {
+        offsetCounter -= itemsPerPage;
+        limitCounter = Math.min(itemsPerPage, totalItems - offsetCounter);
+        pageCounter -= 1;
+        printComic(offsetCounter, limitCounter, pageCounter);
+    }
+});
+
+
+just(".btns-last-page").addEventListener("click", () => {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const startOffset = Math.max(0, (totalPages - 1) * itemsPerPage);
+    const endOffset = totalItems;
+    offsetCounter = startOffset;
+    limitCounter = endOffset - startOffset;
+
+    printComic(offsetCounter, limitCounter, totalPages);
+});
+
+
+
+
