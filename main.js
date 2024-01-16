0e28da92b27a508cc2a137115d7c90eec32d9189
MariaSolGonzalez <solgonzalez24@hotmail.com>
2024-01-15 22:06:01 -0300

    show characters instead of comics functionality has ben finished

--- main.js
+++ main.js
@@ -1,138 +1,183 @@
-//! UTILITIES
+//SECTION
+//ASK
+//TRASHfasd;flakdjsf
+//FIX fdsafkjasdf
+//CHECK dfasdfa
+
+
+
+
+
+//ASK porque no puedo ver con la info de la api en mi celu -tema con el deploy- 
+//ASK linea de la api rompe todo el tiempo y la necesito, no se como hacer para que el scope sea global
+
+
+
+
+//SECTION UTILITIES
 const just = (selector) => document.querySelector(selector);
 
-//! GLOBAL FUNCTIONS
-const urlBase = `http://gateway.marvel.com/v1/public/`;
+//SECTION GLOBAL FUNCTIONS
 let ts = `ts=1`;
 const publicKey = "&apikey=d3abb539098557030e53849b9dc73d81";
 const privateKey = "338e6aeedb37d02e56329d2837e6679586d0c53f";
-const hash = "&hash=56d3e07a1a8a5d3e0602e841365c58f5";
+const hash = "hash=56d3e07a1a8a5d3e0602e841365c58f5";
+let lowerCaseSearchValue = ""
 
-//!global functions for pagination
+
+//SECTION global functions for pagination
 const itemsPerPage = 20
 let offsetCounter = 0;
 let limitCounter = 20;
 let pageCounter = 1
-let totalItems 
+let totalItems
 
-//!DATA FROM API
+
+//SECTION  DATA FROM API
 const getMarvelComics = async (typeSelected, nameSearched, orderSelected, offset, limit, pageNum) => {
 
     let buildingURL = `http://gateway.marvel.com/v1/public/`
 
-    if(typeSelected === "characters"){
-        buildingURL += `${typeSelected}?${ts}${publicKey}&${hash}&offset=${offset}&limit=${limit}&`
+    if (typeSelected === "comics") {
+        if (nameSearched || typeSelected || orderSelected) {
+            buildingURL += `${typeSelected}?${ts}${publicKey}&${hash}&offset=${offset}&limit=${limit}&`
+            if (typeSelected === "comics") {
+                buildingURL += `formatType=${typeSelected}&`
+            }
+            if (nameSearched) {
+                buildingURL += `titleStartsWith=${nameSearched}&`
+            }
+            if (orderSelected === "a-z") {
+                buildingURL += `orderBy=title&`
+            }
+            if (orderSelected === "z-a") {
+                buildingURL += `orderBy=-title&`
+            }
+            if (orderSelected === "newest") {
+                buildingURL += `orderBy=-focDate&`
+            }
+            if (orderSelected === "oldest") {
+                buildingURL += `orderBy=focDate&`
+            }
+        }
+    } else if (typeSelected === "characters") {
+        if (nameSearched || typeSelected || orderSelected) {
+            buildingURL += `${typeSelected}?${ts}${publicKey}&${hash}&offset=${offset}&limit=${limit}&`
+            if (nameSearched) {
+                buildingURL += `nameStartsWith=${nameSearched}&`
+            }
+            if (orderSelected === "a-z") {
+                buildingURL += `orderBy=name&`
+            }
+            if (orderSelected === "z-a") {
+                buildingURL += `orderBy=-name&`
+            }
+            if (orderSelected === "newest") {
+                buildingURL += `orderBy=-focDate&`
+            }
+            if (orderSelected === "oldest") {
+                buildingURL += `orderBy=focDate&`
+            }
+        }
     }
-    if(nameSearched || typeSelected || orderSelected){
-        buildingURL += `${typeSelected}?`
-    if(typeSelected === "comics"){
-        buildingURL += `formatType=${typeSelected}&${ts}${publicKey}&${hash}&offset=${offset}&limit=${limit}&`
-    }
-    if(nameSearched && (typeSelected === "comics")){
-        buildingURL += `titleStartsWith=${nameSearched}&offset=${offset}&limit=${limit}&`
-    }
-    if((nameSearched && (typeSelected === "characters"))){
-        buildingURL += `nameStartsWith=${nameSearched}&offset=${offset}&limit=${limit}&`
-    }
-    if(orderSelected === "a-z" && (typeSelected === "comics")){
-        buildingURL += `orderBy=title&offset=${offset}&limit=${limit}&`
-    }
-    if(orderSelected === "a-z" && (typeSelected === "characters")){
-        buildingURL += `orderBy=name&offset=${offset}&limit=${limit}&`
-    }
-    if(orderSelected === "z-a" && (typeSelected === "comics")){
-        buildingURL += `orderBy=-title&offset=${offset}&limit=${limit}&`
-    }
-    if(orderSelected === "z-a" && (typeSelected === "characters")){
-        buildingURL += `orderBy=-name&offset=${offset}&limit=${limit}&`
-    }
-    if(orderSelected === "newest"){
-        buildingURL += `orderBy=-focDate&offset=${offset}&limit=${limit}&`
-    }
-    if (orderSelected === "oldest"){
-        buildingURL += `orderBy=focDate&offset=${offset}&limit=${limit}&`
-    }
-    }
+    console.log(buildingURL);
     const response = await fetch(buildingURL);
     const data = await response.json();
     totalItems = data.data.total
     return data;
 };
 
-//!RENDER COMICS
+
+//SECTION RENDER COMICS
 const printComic = async (typeSelected, nameSearched, orderSelected, offset, limit, pageNum) => {
-  const comics = await getMarvelComics(typeSelected, nameSearched, orderSelected, offset, limit, pageNum);
-  just(".results-cards-comics").innerHTML = ``;
-  just(".total-of-comics").innerText = `${comics.data.total} RESULTADOS`;
-  just(".current-page-num").innerText = `page ${pageNum}`
-
-  for (let comic of comics.data.results) {
-    let pathImg = comic.thumbnail.path + "/portrait_incredible.jpg";
-
-    just(".results-cards-comics").innerHTML += `
-        <div class="div-cards p-4 ">
-            <div class="div-img-cover-magazine ">
-                <img src="${pathImg}">
+    const comics = await getMarvelComics(typeSelected, nameSearched, orderSelected, offset, limit, pageNum);
+    console.log(comics.data.results);
+    just(".results-cards-comics").innerHTML = ``;
+    just(".total-of-comics").innerText = `${comics.data.total} RESULTADOS`;
+    just(".current-page-num").innerText = `page ${pageNum}`
+    console.log(typeSelected);
+    for (let comic of comics.data.results) {
+        let pathImgComic = comic.thumbnail.path + "/portrait_incredible.jpg";
+        if (typeSelected === "comics") {
+            just(".results-cards-comics").innerHTML += `
+            <div class="div-cards">
+                <div class="div-img-cover-magazine ">
+                    <img src="${pathImgComic}">
+                </div>
+                <p class="text-black font-semibold mt-6">${comic.title}</p>
             </div>
-            <p class="font-semibold mt-6">${comic.title}</p>
-        </div>
-        `;
-  }
+            `;
+        }
+        else if(typeSelected === "characters"){
+            just(".results-cards-comics").innerHTML += `
+            <div class="div-cards characters bg-black text-center m-4">
+                <div class="div-img-cover-magazine ">
+                    <img src="${pathImgComic}">
+                </div>
+                <p class="text-white font-semibold mt-6">${comic.name}</p>
+            </div>
+            `
+        }
+    }
 };
 printComic("comics", null, "a-z", offsetCounter, limitCounter, pageCounter);
 
 
 
-//!FUNCTIONALITY FOR FILTERS
-//?BY INPUT
+
+//SECTION FUNCTIONALITY FOR FILTERS
+//*BY INPUT
 just(".search-input").addEventListener("input", () => {
+
+    lowerCaseSearchValue = just(".search-input").value.toLowerCase();
     printComic(
         just(".type-filter").value,
-        just(".search-input").value,
+        lowerCaseSearchValue,
         just(".order-filter").value,
         offsetCounter,
         limitCounter,
         pageCounter)
-})
+});
 
-//? BY TYPE (CATEGORY - CHARACTER)
+
+//* BY TYPE (COMIC - CHARACTER)
 just(".type-filter").addEventListener("change", () => {
     printComic(
         just(".type-filter").value,
-        just(".search-input").value,
+        lowerCaseSearchValue,
         just(".order-filter").value,
         offsetCounter,
         limitCounter,
         pageCounter)
 })
 
-//?BY ORDER (ABC - NEWEST - OLDEST)
+//*BY ORDER (ABC - NEWEST - OLDEST)
 just(".order-filter").addEventListener("change", () => {
     printComic(
         just(".type-filter").value,
-        just(".search-input").value,
+        lowerCaseSearchValue,
         just(".order-filter").value,
         offsetCounter,
         limitCounter,
         pageCounter)
 })
 
-//!FUNCTIONALITY FOR PAGINATION
-//?FIRST PAGE
-just(".btns-first-page").addEventListener("click", ()=> {
+//SECTION FUNCTIONALITY FOR PAGINATION
+//*FIRST PAGE
+just(".btns-first-page").addEventListener("click", () => {
     offsetCounter = 0
     limitCounter = 20
     pageCounter = 1
     printComic(
         just(".type-filter").value,
-        just(".search-input").value,
+        lowerCaseSearchValue,
         just(".order-filter").value,
         offsetCounter,
         limitCounter,
         pageCounter)
 })
 
-//?PREVIOUS PAGE
+//*PREVIOUS PAGE
 just(".btns-prev-pag").addEventListener("click", () => {
     if (offsetCounter >= itemsPerPage) {
         offsetCounter -= itemsPerPage;
@@ -141,30 +186,30 @@ just(".btns-prev-pag").addEventListener("click", () => {
     }
     printComic(
         just(".type-filter").value,
-        just(".search-input").value,
+        lowerCaseSearchValue,
         just(".order-filter").value,
         offsetCounter,
         limitCounter,
         pageCounter)
-    });
+});
 
-//?NEXT PAGE
+//*NEXT PAGE
 just(".btns-next-page").addEventListener("click", () => {
     if (offsetCounter + itemsPerPage < totalItems) {
         offsetCounter += itemsPerPage;
         limitCounter = Math.min(itemsPerPage, totalItems - offsetCounter);
-        pageCounter +=1
+        pageCounter += 1
     }
     printComic(
         just(".type-filter").value,
-        just(".search-input").value,
+        lowerCaseSearchValue,
         just(".order-filter").value,
         offsetCounter,
         limitCounter,
         pageCounter)
 })
 
-//?LAST PAGE
+//*LAST PAGE
 just(".btns-last-page").addEventListener("click", () => {
     pageCounter = Math.ceil(totalItems / itemsPerPage);
     const startOffset = Math.max(0, (pageCounter - 1) * itemsPerPage);
@@ -173,7 +218,7 @@ just(".btns-last-page").addEventListener("click", () => {
     limitCounter = endOffset - startOffset;
     printComic(
         just(".type-filter").value,
-        just(".search-input").value,
+        lowerCaseSearchValue,
         just(".order-filter").value,
         offsetCounter,
         limitCounter,
@@ -183,9 +228,34 @@ just(".btns-last-page").addEventListener("click", () => {
 
 
 
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH TRASH
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const filtersUrl = (typeSelected, nameSearched, orderSelected, offset, limit, pageNum) =>{
-//     console.log("me ejecuto");
+// //     console.log("me ejecuto");
 //     let buildingURL = `http://gateway.marvel.com/v1/public/`
 //     if(typeSelected === "characters"){
 //         buildingURL += `${typeSelected}?${ts}${publicKey}&${hash}&` // http://gateway.marvel.com/v1/public/characters?ts=1&apikey=d3abb539098557030e53849b9dc73d81&hash=56d3e07a1a8a5d3e0602e841365c58f5
